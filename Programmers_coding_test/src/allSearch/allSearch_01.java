84c6f7ea8a9d6e8a742ed9d2e5b82f62f7dc5052
ironmask431 <ironmask431@gmail.com>
2022-01-24 20:45:02 +0900

    코딩테스트 01.24 커밋

---
+++ Programmers_coding_test/src/allSearch/allSearch_01.java
@@ -0,0 +1,109 @@
+package allSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * 코딩테스트연습 > 완전탐색 > 모의고사 (LV.1) (2022.01.24 통과)
+ * 
+ * 문제 설명
+수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한 조건
+시험은 최대 10,000 문제로 구성되어있습니다.
+문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+입출력 예
+answers	return
+[1,2,3,4,5]	[1]
+[1,3,2,4,2]	[1,2,3]
+입출력 예 설명
+입출력 예 #1
+
+수포자 1은 모든 문제를 맞혔습니다.
+수포자 2는 모든 문제를 틀렸습니다.
+수포자 3은 모든 문제를 틀렸습니다.
+따라서 가장 문제를 많이 맞힌 사람은 수포자 1입니다.
+
+입출력 예 #2
+
+모든 사람이 2문제씩을 맞췄습니다.
+ */
+public class allSearch_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] answers = {1,2,3,4,5};
+//		int[] answers = {1,3,2,4,2};
+
+		solution(answers);
+	}
+	
+	public static int[] solution(int[] answers) {
+        int[] answer = {};
+        
+        //참가자 1,2,3번의 문제찍는 반복패턴 설정
+        int[] man_1 = {1, 2, 3, 4, 5}; //0 ~ 4 // i%5 =     
+        int[] man_2 = {2, 1, 2, 3, 2, 4, 2, 5}; // 0 ~ 7  // 1%8 = 
+        int[] man_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //0 ~ 9  // 1%10 = 
+                    //{1, 2, 3, 4, 5, 1, 2, 3, 4 ,5};
+        
+        //참가자 1,2,3의 정답맞춘 횟수
+        int man_1_cnt = 0;
+        int man_2_cnt = 0;
+        int man_3_cnt = 0;
+        
+        //전체 문제정답 for문
+        for(int i = 0 ; i < answers.length; i++) {                 
+            
+            //(현재문제 정답) 과  (현재 문제순번 % 문제찍는패턴의 길이 = 현재찍은 정답)을 비교. 일치하면 cnt++; 
+            if(answers[i] == man_1[i%man_1.length]) {
+                man_1_cnt++;
+            }            
+            if(answers[i] == man_2[i%man_2.length]) {
+                man_2_cnt++;
+            }      
+            if(answers[i] == man_3[i%man_3.length]) {
+                man_3_cnt++;
+            }    
+        }      
+   
+        //각 참가자의 정답횟수를 test에 입력 
+        int[] test = {man_1_cnt, man_2_cnt, man_3_cnt};
+        
+        //오름차순 정리 
+        Arrays.sort(test);
+ 
+        List<Integer> list = new ArrayList<Integer>();
+   
+        //test 리스트의 마지막 수(최대값)과 각 참가자의 정답횟수가 일치하면 최대정답자. 최대정답자를 list에 add
+        if(test[test.length-1] == man_1_cnt) {
+            list.add(1);
+        }
+        if(test[test.length-1] == man_2_cnt) {
+            list.add(2); 
+        }
+        if(test[test.length-1] == man_3_cnt) {
+            list.add(3);
+        }
+        
+        //list를 answer[] 배열로 변환     
+        answer = new int[list.size()];
+        
+        for(int i=0 ; i<list.size(); i++) {
+            answer[i] = list.get(i);
+            //System.out.println("정답:"+answer[i]);
+        }
+        
+        return answer;
+    }
+}
+
--- Programmers_coding_test/src/sort/sort_01.java
+++ Programmers_coding_test/src/sort/sort_01.java
@@ -3,20 +3,30 @@ package sort;
 import java.util.Arrays;
 
 /**
- *  프로그래머스 - 코딩테스트연습 - 정렬 - K번째 수
- *  
- * 	배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+ * 코딩테스트연습 > 정렬 > k번째수 (Lv.1) (2022.01.23 통과)
+ * 
+ * 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
 	예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
 	
 	array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
 	1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
 	2에서 나온 배열의 3번째 숫자는 5입니다.
-	배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 
-	설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+	배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 
+	대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 	
-	일출력 예시)
-	array			commands				return
+	제한사항
+	array의 길이는 1 이상 100 이하입니다.
+	array의 각 원소는 1 이상 100 이하입니다.
+	commands의 길이는 1 이상 50 이하입니다.
+	commands의 각 원소는 길이가 3입니다.
+	입출력 예
+	array	commands	return
 	[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+	입출력 예 설명
+	[1, 5, 2, 6, 3, 7, 4]를 2번째부터 5번째까지 자른 후 정렬합니다. [2, 3, 5, 6]의 세 번째 숫자는 5입니다.
+	[1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
+	[1, 5, 2, 6, 3, 7, 4]를 1번째부터 7번째까지 자릅니다. [1, 2, 3, 4, 5, 6, 7]의 세 번째 숫자는 3입니다.
+ *  
  */
 
 public class sort_01 {
@@ -31,30 +41,30 @@ public class sort_01 {
 	}
 	
 	public static int[] solution(int[] array, int[][] commands) {
+		
 		int[] answer = {};
 	
-		//Arrays.asList 는 배열이 String 배열 일경우만 제대로 동작함. int 배열에서는 되지않는 것 같음.
-		//ArrayList<Integer> list = new ArrayList(Arrays.asList(array));
-		
 		answer = new int[commands.length];
 		
+		//{2, 5, 3} 을 예시로 주석설명
 		for(int i = 0; i < commands.length; i++) {
 			int int_A = commands[i][0]; //2
 			int int_B = commands[i][1]; //5
 			int int_C = commands[i][2]; //3
 			
-			//기존 배열을 잘라서 (2번째자리 ~ 5번째자리) 새로운 배열 만듬.
+			//2번째 ~ 5번째 숫자로 된 새로운 배열 생성 
 			int[] newArray = Arrays.copyOfRange(array, int_A-1, int_B);
-			
-			//새로운 배열 오름차순 정렬
+
+			//새로운 배열 오름차순 정리
 			Arrays.sort(newArray);
 			
-			//새로운 배열의 3번째 자리 수 확인
+			//새로운 배열의 3번째 자리 숫자 확인
 			System.out.println("정답="+newArray[int_C-1]);
-
-			//정답용 배열에 정답입력
+			
+			//정답 배열에 정답 입력
 			answer[i]=newArray[int_C-1];	
 		}
+		
         return answer;
     }
 }
