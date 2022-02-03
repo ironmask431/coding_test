197a4b026759da1ebd8373f15ea0b954e0a062ec
ironmask431 <ironmask431@gmail.com>
2022-02-03 19:43:47 +0900

    코딩테스트연습 > 스택/큐 > 기능개발 (Lv.2) (2022.02.03 통과)

--- Programmers_coding_test/src/hash/hash_01.java
+++ Programmers_coding_test/src/hash/hash_01.java
@@ -6,13 +6,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 
-/* 코딩테스트연습 > 해시 > 완주하지 못한 선수 (LV.1) (2022.01.23 통과)
- * 
- * 
+/* 
+코딩테스트연습 > 해시 > 완주하지 못한 선수 (LV.1) (2022.01.23 통과)
+
 문제 설명
 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
 
-마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 
 제한사항
 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
@@ -33,7 +34,6 @@ participant	completion	return
 
 예제 #3
 "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.	
-	
  */
 public class hash_01 {
 
---
+++ Programmers_coding_test/src/stack_queue/stack_queue_01.java
@@ -0,0 +1,99 @@
+package stack_queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+코딩테스트연습 > 스택/큐 > 기능개발 (Lv.2) (2022.02.03 통과)
+문제 설명
+프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 
+기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 
+주어질 때 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+
+제한 사항
+작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+작업 진도는 100 미만의 자연수입니다.
+작업 속도는 100 이하의 자연수입니다.
+배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 
+진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
+입출력 예
+progresses	speeds	return
+[93, 30, 55]	[1, 30, 5]	[2, 1]
+[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+입출력 예 설명
+입출력 예 #1
+첫 번째 기능은 93% 완료되어 있고 하루에 1%씩 작업이 가능하므로 7일간 작업 후 배포가 가능합니다.
+두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다. 하지만 이전 
+첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
+세 번째 기능은 55%가 완료되어 있고 하루에 5%씩 작업이 가능하므로 9일간 작업 후 배포가 가능합니다.
+
+따라서 7일째에 2개의 기능, 9일째에 1개의 기능이 배포됩니다.
+
+입출력 예 #2
+모든 기능이 하루에 1%씩 작업이 가능하므로, 작업이 끝나기까지 남은 일수는 각각 5일, 10일, 1일, 1일, 20일, 1일입니다. 
+어떤 기능이 먼저 완성되었더라도 앞에 있는 모든 기능이 완성되지 않으면 배포가 불가능합니다.
+따라서 5일째에 1개의 기능, 10일째에 3개의 기능, 20일째에 2개의 기능이 배포됩니다.
+*/
+
+public class stack_queue_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int[] p = {93, 30, 55}; //작업진도
+//		int[] s = {1, 30, 5}; //작업속도
+		
+		int[] p = {95, 90, 99, 99, 80, 99}; //작업진도
+		int[] s = {1, 1, 1, 1, 1, 1}; //작업속도
+		
+		solution(p,s);
+	}
+	
+	public static int[] solution(int[] progresses, int[] speeds){
+		int[] answer = {};
+		
+		//큐 생성
+		Queue<Integer> q = new LinkedList();
+		
+		//작업진도 배열을 큐에 넣음
+		for(int i : progresses) {
+			q.offer(i);
+		}
+		
+		int day = 0; //진행일자
+		int done = 0; //완료된 큐 숫자
+		
+		//answer를 담을 list 생성
+		List<Integer> list = new ArrayList<Integer>();
+		
+		//큐가 다 소모될때까지
+		while(q.peek() != null) {
+			int answerNum = 0; //한번에 완료된 작업의 개수 (answer에 담을 숫자)
+			day++; //진행일자++
+			
+			//현재 큐의 첫번째 작업진도 + 해당 작업의 작업속도x진행일자가 100이상일경우 poll()로 작업진도를 꺼낸다.
+			while(q.peek()!= null && q.peek() + (speeds[done]*day) >= 100) {
+				q.poll();
+				done++; //작업완료된 큐 숫자 ++
+				answerNum ++; //한번에 완료된 작업의 개수 ++
+			}
+			//한번에 완료된 작업의 개수가 1이상일 경우 list에 담는다.
+			if(answerNum > 0) {
+				list.add(answerNum);
+			}
+		}
+//		System.out.println(list.toString());
+		
+		//list를 answer 배열로 변환 
+		answer = new int[list.size()];
+		for(int i=0 ; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		
+        return answer;
+    }
+}
