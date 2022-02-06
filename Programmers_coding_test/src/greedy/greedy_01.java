eee735f1cad833c6531301e08b564332040a79d6
ironmask431 <ironmask431@gmail.com>
2022-02-06 16:35:38 +0900

    2022.02.06

---
+++ Programmers_coding_test/src/greedy/greedy_01.java
@@ -0,0 +1,116 @@
+package greedy;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+코딩테스트연습 > 탐욕법(Greedy) > 체육복 (LV.1) (2022.02.06 완료)
+
+문제설명
+점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다.
+다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 
+학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 
+뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 
+3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 
+수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 
+여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
+체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+전체 학생의 수는 2명 이상 30명 이하입니다.
+체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 
+하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+
+입출력 예
+n	lost	reserve	return
+5	[2, 4]	[1, 3, 5]	5
+5	[2, 4]	[3]	4
+3	[3]	[1]	2
+
+입출력 예 설명
+예제 #1
+1번 학생이 2번 학생에게 체육복을 빌려주고, 3번 학생이나 5번 학생이 4번 학생에게 체육복을 빌려주면 학생 5명이 체육수업을 들을 수 있습니다.
+
+예제 #2
+3번 학생이 2번 학생이나 4번 학생에게 체육복을 빌려주면 학생 4명이 체육수업을 들을 수 있습니다.
+	
+ */
+public class greedy_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5;
+		int[] lost = {2,4}; 
+		int[] reserve = {1,3,5};
+		
+		solution(n,lost,reserve);
+		
+	}
+	
+	public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        //학생 전체 명단 입력 (체육복1벌)
+        for(int i = 1; i <= n; i++) {
+        	map.put(i, 1);
+        }
+        
+        //여벌체육복 명단은 체육복+1 
+        for(int i : reserve) {
+        	map.put(i, map.get(i)+1);
+        }
+        
+        //체육복 분식 명단은 체육복-1
+        for(int i : lost) {
+        	map.put(i, map.get(i)-1);
+        } 
+        
+        Iterator<Integer> it = map.keySet().iterator();
+        
+        while(it.hasNext()) {
+        	int key = it.next(); //현재 학생번호 
+        	int pre = key-1; //이전 번호
+        	int next = key+1; //다음 번호
+        	
+        	//현재 학생의 체육복이 0벌이고 이전번호 or 다음번호가 여벌이있으면 체육복 1벌 빌림.
+        	if(map.get(key) == 0){
+        		if(map.get(pre) != null && map.get(pre) == 2) {
+        			map.put(key, 1);
+        			map.put(pre, 1);
+        		}	
+        	}
+        	
+        	if(map.get(key) == 0){
+        		if(map.get(next) != null && map.get(next) == 2) {
+        			map.put(key, 1);
+        			map.put(next, 1);
+        		}	
+        	}
+        }
+
+        it = map.keySet().iterator();
+        
+        while(it.hasNext()) {
+        	int key = it.next();
+        	
+        	//체육복1벌이상인 명단 수를 answer에 입력
+        	if(map.get(key) > 0) {
+        		answer++;
+        	}
+        }
+  
+        System.out.println(answer);
+        
+        return answer;
+    }
+
+}
