b2b3d148f2fd67915e017059ffd84359b51642bc
ironmask431 <48856906+ironmask431@users.noreply.github.com>
2022-02-10 22:32:54 +0900

    Update sort_02.java

--- Programmers_coding_test/src/sort/sort_02.java
+++ Programmers_coding_test/src/sort/sort_02.java
@@ -18,9 +18,10 @@ import java.util.Arrays;
 numbers의 길이는 1 이상 100,000 이하입니다.
 numbers의 원소는 0 이상 1,000 이하입니다.
 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+
 입출력 예
-numbers	return
-[6, 10, 2]	"6210"
+numbers			return
+[6, 10, 2]		"6210"
 [3, 30, 34, 5, 9]	"9534330"
 
  */
