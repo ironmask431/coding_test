119d4193c11f77882d3ceeb3e9d17fd6c235a3cb
ironmask431 <ironmask431@gmail.com>
2022-02-21 23:19:30 +0900

    2022.02.21

---
+++ Programmers_coding_test/src/greedy/greedy_02.java
@@ -0,0 +1,28 @@
+package greedy;
+
+
+/* 
+코딩테스트연습 > 탐욕법(Greedy) > 큰 수 만들기 (LV.2)
+	
+ */
+public class greedy_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String number = "1924";
+		int k = 2;
+		
+		solution(number, k);
+		
+	}
+	
+	public static String solution(String number, int k) {
+        String answer = "";
+        
+        
+        
+        return answer;
+    }
+
+}
