283fb1b6af53df29a4cf0ac7a28459843b3a3050
ironmask431 <ironmask431@gmail.com>
2022-03-16 22:16:09 +0900

    2022.03.16

---
+++ Programmers_coding_test/src/hash/hash_04.java
@@ -0,0 +1,22 @@
+package hash;
+
+/* 
+
+*/
+
+public class hash_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] genres = {};
+		int[] plays = {};
+		solution(genres, plays);
+	}
+	
+	public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
